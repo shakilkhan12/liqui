7610a6ea5d2ebb5cd5f5723132397b1fba8cadb4
shakilkhan12 <shakilkhan621@gmail.com>
2024-03-07 23:25:11 +0500

    banner changed again

--- components/Banner.js
+++ components/Banner.js
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <header className="bg-[url('/images/banner.png')] w-full h-[480px] lg:h-[788px] bg-no-repeat bg-cover bg-center"></header>
+    <header className="bg-[url('/images/banner.png')] w-full h-[450px] md:h-[600px] lg:h-[788px] bg-no-repeat bg-cover bg-center"></header>
   );
 };
 
